c91d39677ef8640c98bae6f97cac669e14be069d
Pranav-byte-pro <Pranavambre2001@gmail.com>
2023-05-07 20:20:23 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IntershipCode_bank_login.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(string username ,string password)
+        {
+
+            if(username=="Abhi@relationshipManeger" && password=="Relationship@123")
+            {
+                return RedirectToAction("RelationshipManager"); 
+            }
+            else if(username == "Abhi@operationalManeger" && password == "Relationship@123")
+            {
+                return RedirectToAction("OperationalManager");
+            }
+            else
+            {
+                return RedirectToAction("Error");
+            }
+        }
+        public ActionResult LoginPage()
+        {
+            return View();  
+        }
+        public ActionResult SignUpPage()
+        {
+            return View();
+        }
+        public ActionResult RelationshipManager()
+        {
+            return View();
+        }
+        public ActionResult OperationalManager()
+        {
+            return View();
+        }
+
+        public ActionResult CreateCustomer()
+        {
+            return View();
+        }
+
+        public ActionResult ProductMetrics()
+        {
+            return View();
+        }
+        public ActionResult OrderManagement()
+        {
+            return View();
+
+        }
+
+        public ActionResult CustomerSupport()
+        {
+            return View();
+
+        }
+
+        public ActionResult EmployeeManagement()
+        {
+            return View();
+
+        }
+
+        public ActionResult Error()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="IntershipCode_bank_login.MvcApplication" Language="C#" %>
